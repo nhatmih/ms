1d0f407ea6dc9998a928a7fda93f9696badf1281
QuynhAnh <huynhanh.170504@gmail.com>
2023-03-12 20:37:58 +0700

    update hello.cpp again

--- hello.cpp
+++ hello.cpp
@@ -2,16 +2,8 @@
 
 using namespace std;
 
-int gcd(int a, int b) {
-    while(a != b) {
-        if(a > b) a-= b;
-        else b -= a;
-    }
-    return a;
-}
-
 int main(){
-    cout<<"remember to merge before push" << endl;
+    cout<<"remember to merge your own branch to main before push" << endl;
     cout<<"Hello World !";
     return 0;
 }
