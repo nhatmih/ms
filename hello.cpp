697db9041ac8c2876f0dba48d34dec6cc915a275
QuynhAnh <huynhanh.170504@gmail.com>
2023-03-12 23:24:28 +0700

    update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    cout<<"remember to merge your own branch to main before push" << endl;
-    cout<<"Hello World !";
+    cout << "remember to merge your own branch to main before push" << endl;
+    cout << "Hello World !";
     return 0;
 }
